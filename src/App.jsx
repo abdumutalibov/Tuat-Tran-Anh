368dab17510047ff8bcaa24bdd6cc29b3bf6576c
abdumutalibov <azimabdumutalibov21@gmail.com>
2022-01-16 00:05:22 +0500

    yoct

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-
-
+import "./assets/libs/boxicons-2.1.1/css/boxicons.min.css";
+import "./scss/App.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Blank from "./pages/Blank";
+import Dashboard from "./pages/Dashboard";
+import MainLayout from "./layout/MainLayout";
 function App() {
   return (
-    <div className="">
-    d
-  </div>
-
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainLayout />} >
+        <Route index element={<Dashboard />} />
+        <Route path="orders" element={<Blank />} />
+        <Route path="products" element={<Blank />} />
+        <Route path="customers" element={<Blank />} />
+        <Route path="settings" element={<Blank />} />
+        <Route path="stats" element={<Blank />} />
+     </Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/summary-box/SummaryBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './summary-box.scss'
+const SummaryBox = ({item}) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SummaryBox
