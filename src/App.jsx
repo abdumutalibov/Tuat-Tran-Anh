0d5980182f77250cc02df3e773098217cd9d9b98
abdumutalibov <azimabdumutalibov21@gmail.com>
2022-01-20 20:34:03 +0500

    sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./assets/libs/boxicons-2.1.1/css/boxicons.min.css";
+import "./assets/boxicons-2.1.1/css/boxicons.min.css";
 import "./scss/App.scss";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Blank from "./pages/Blank";
